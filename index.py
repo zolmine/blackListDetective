1b0f9a60795ab202bc8e67ff8e5a72f9569aa243
zolmine <68522174+zolmine@users.noreply.github.com>
2022-02-20 11:09:38 +0000

    creation of the index file

---
+++ index.py
@@ -0,0 +1,35 @@
+from websocket import create_connection
+from json import dumps
+import os,sys
+from concurrent.futures import ThreadPoolExecutor
+executor = ThreadPoolExecutor(max_workers=4)
+
+try:
+    url     = 'wss://polygon-mainnet.g.alchemy.com/v2/tGb32Cyl35DXFXXlOuJqbAAf6MbXPn0o'
+
+    filtre = {"jsonrpc":"2.0","id": 0, "method": "eth_subscribe", "params": ["alchemy_filteredNewFullPendingTransactions", {"address": "0xa5e0829caced8ffdd4de3c43696c57f7d7a678ff"}]},{"jsonrpc":"2.0","id": 1, "method": "eth_subscribe", "params": ["alchemy_filteredNewFullPendingTransactions", {"address": "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506"}]}
+
+    json_data = dumps(filtre).encode("utf-8")
+
+
+    ws = create_connection(url)
+
+    print("connected")
+
+    ws.send(json_data)
+
+    while True:
+        try:
+            response = ws.recv()
+            # args = response,ws
+            executor.submit(dataFilter,[response,ws])
+
+        except:
+            continue
+
+except Exception as err:
+
+    exc_type, exc_obj, exc_tb = sys.exc_info()
+    fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+    print(str([exc_type,fname,exc_tb.tb_lineno,err]))
+    
